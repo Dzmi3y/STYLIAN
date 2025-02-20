b1cc180b73c6228cd8d2f12f13bfb26ace326394
Dzmitry <dzmi3y@gmail.com>
2025-02-20 17:00:43 +0300

    custom-media queries added

--- stylian.client/postcss.config.mjs
+++ stylian.client/postcss.config.mjs
@@ -1,7 +1,13 @@
+const postcss = require("postcss");
+const postcssCustomMedia = require("postcss-custom-media");
+const postcssImport = require("postcss-import");
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
     tailwindcss: {},
+    "postcss-import": {},
+    "postcss-custom-media": {},
   },
 };
 
