74a6636ac628b184f46b05783453fe94c47150a3
Dzmitry <dzmi3y@gmail.com>
2025-02-25 21:57:46 +0300

    Mobile nav menu added

--- stylian.client/src/components/Buttons/MenuButton/MenuButton.tsx
+++ stylian.client/src/components/Buttons/MenuButton/MenuButton.tsx
@@ -1,24 +1,48 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import BaseButton from "../BaseButton/BaseButton";
 import Image from "next/image";
 import styles from "./MenuButton.module.css";
+import { ROUTES } from "@/data/const/routes";
+import NavLink from "@/components/NavLink/NavLink";
 
 const MenuButton = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
   const menuClickHandler = () => {
-    console.log("menuClickHandler");
+    setIsMenuOpen(!isMenuOpen);
   };
   return (
     <div className={styles.container}>
       <BaseButton onClick={menuClickHandler}>
         <Image
+          className={isMenuOpen ? styles.showImage : styles.hideImage}
           src="/images/icons/menu_icon.png"
-          alt="Next.js logo"
+          alt="menu button"
+          width={30}
+          height={20}
+          priority
+        />
+        <Image
+          className={!isMenuOpen ? styles.showImage : styles.hideImage}
+          src="/images/icons/close_icon.png"
+          alt="menu button"
           width={30}
           height={20}
           priority
         />
       </BaseButton>
+      <div
+        className={`${styles.popup} ${
+          !isMenuOpen ? styles.showPopup : styles.hidePopup
+        }`}
+      >
+        <nav className={styles.nav}>
+          <NavLink url={ROUTES.HOME}>Home</NavLink>
+          <NavLink url={ROUTES.CATALOG}>Catalog</NavLink>
+          <NavLink url={ROUTES.CONTACTS}>Contacts</NavLink>
+        </nav>
+      </div>
     </div>
   );
 };
